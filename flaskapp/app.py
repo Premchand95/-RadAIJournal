aa8e04fe60a3759b6264cd5598682a2ac20c6a04
Premchand95 <35117193+Premchand95@users.noreply.github.com>
2018-01-09 21:20:05 -0500

    Add files via upload

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template,flash,redirect,url_for,session,logging,request
-from flask_mysqldb import MySQL
-from wtforms import Form, StringField, TextAreaField, PasswordField, validators,SelectField
+from flask_mysql import MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators,SelectField,RadioField,FloatField,SelectMultipleField, widgets
 from passlib.hash import sha256_crypt
 from functools import wraps
 app=Flask(__name__)
+
+class MultiCheckboxField(SelectMultipleField):
+  widget = widgets.ListWidget(prefix_label=False)
+  option_widget = widgets.CheckboxInput()
 
 # Config MySQL
 app.config['MYSQL_HOST'] = 'localhost'
@@ -27,34 +31,47 @@ def contact():
     return render_template('contact.html')
 # Register Form Class
 class RegisterForm(Form):
-    name = StringField('Name',[validators.Length(min=1, max=50)])
-    username = StringField('Username', [validators.Length(min=4, max=25)])
+    first_name = StringField('First Name',[validators.Length(min=1, max=50),validators.DataRequired()])
+    middle_name = StringField('Middle Name')
+    last_name = StringField('Last Name',[validators.Length(min=1, max=50),validators.DataRequired()])
+    npi = FloatField('NPI',[validators.DataRequired(),validators.DataRequired()])
+    doctor = SelectField('Doctor',choices=[('a', 'MD/DO'), ('b', 'MBBS'), ('c', 'MBchB'),('d', 'I am not a doctor')])
+    radiologist = RadioField('Radiologist',choices=[('yes', 'Radiologist'), ('no', 'Non Radiologist')])
+    training = SelectField('Training', choices=[('staff','Staff'),('r0','Resident-R0/PGY 1'),('r1','Resident-R1/PGY 2'),('r2','Resident-R2/PGY 3'),('r3','Resident-R3/PGY 4'),('r4','Resident-R4/PGY 5')])
+    clinical_practice = RadioField('Clinical practice',choices=[('1', '<5 years'),('2', '5-10 years'),('3', '10-15 years'),('4', '15-20 years'),('5', '>20 years')])
+    institution_type = RadioField('Institution type',choices=[('1', 'Private practice'), ('0', 'Academic')])
     email = StringField('Email', [validators.Length(min=6, max=50)])
-    password = PasswordField('Password', [
-        validators.DataRequired(),
-        validators.EqualTo('confirm', message='Passwords do not match')
-    ])
-    confirm = PasswordField('Confirm Password')
-    specialization = SelectField('specialization',choices=[('B', 'Breast'), ('PC', 'Chest/cardio'), ('I', 'IR'),('EG', 'ER/General'),('BAM', 'Body/abdomen/MRI'),('HN', 'Head/Neck')])
-    experience = SelectField('experience', choices=[('0','R0'),('1','R1'),('2','R2'),('3','R3')])
+    clinical_specality = MultiCheckboxField('Clinical Specality', choices=[('Body_Abdomen','Body/Abdomen'),('Head_Neck','Head and Neck'),('Nuclear_Medicine','Nuclear Medicine'),('MSK','MSK'),('Pediatrics','Pediatrics'),('Breast','Breast'),('Chest_Cardiac','Chest/Cardiac'),('Interventional_Radiology','Interventional Radiology'),('ER_General','ER/General')])
+    #password = PasswordField('Password', [validators.DataRequired(),validators.EqualTo('confirm', message='Passwords do not match')])
+    #confirm = PasswordField('Confirm Password')
+
 
 # User Register
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm(request.form)
     if request.method == 'POST' and form.validate():
-        name = form.name.data
+        first_name = form.first_name.data
+        middle_name = form.middle_name.data
+        last_name = form.last_name.data
+        npi=form.npi.data
+        doctor = form.doctor.data
+        radiologist = form.radiologist.data
+        training = form.training.data
+        clinical_practice = form.clinical_practice.data
+        clinical_specality = form.clinical_specality.data
+        institution_type = form.institution_type.data
+        country = request.form['country']
+        state = request.form['state']
         email = form.email.data
-        username = form.username.data
-        password = sha256_crypt.encrypt(str(form.password.data))
-        specialization = form.specialization.data
-        experience = form.experience.data
+        #password = sha256_crypt.encrypt(str(form.password.data))
+        #specialization = form.specialization.data
 
         # Create cursor
         cur = mysql.connection.cursor()
 
         # Execute query
-        cur.execute("INSERT INTO USERS(name, email, username, password,specialization,experience) VALUES(%s, %s, %s, %s,%s,%s)", (name, email, username, password,specialization,experience))
+        cur.execute("INSERT INTO USERS(first_name,middle_name,last_name,npi,doctor,radiologist,training,clinical_practice,clinical_specality,institution_type,country,state,email) VALUES(%s, %s, %s,%f, %s,%s,%s,%s, %s, %s, %s,%s,%s)", (first_name,middle_name,last_name,npi,doctor,radiologist,training,clinical_practice,clinical_specality,institution_type,country,state,email))
 
         # Commit to DB
         mysql.connection.commit()
@@ -115,6 +132,7 @@ def login():
             return render_template('login.html', error=error)
 
     return render_template('login.html')
+
 
 #dashboard
 @app.route('/dashboard')
